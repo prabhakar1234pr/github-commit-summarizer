75d6a325329da551687384bad5dbdeacd171653b
PRABHAKAR ELAVALA <prabhakarpr554@gmail.com>
2026-02-01 23:57:28 -0800

    Initial commit

---
+++ linkedin_oauth.py
@@ -0,0 +1,44 @@
+import os
+import requests
+import webbrowser
+from urllib.parse import urlparse, parse_qs
+
+CLIENT_ID = "864cfuax4q6hds"
+CLIENT_SECRET = os.environ.get("LINKEDIN_CLIENT_SECRET", "")
+REDIRECT_URI = "https://example.com/auth/linkedin/callback"
+
+AUTH_URL = (
+    "https://www.linkedin.com/oauth/v2/authorization"
+    "?response_type=code"
+    f"&client_id={CLIENT_ID}"
+    f"&redirect_uri={REDIRECT_URI}"
+    "&scope=w_member_social"
+    "&state=123456"
+)
+
+print("Opening LinkedIn authorization page...")
+webbrowser.open(AUTH_URL)
+
+redirected_url = input(
+    "\nAfter clicking ALLOW, paste the FULL redirect URL here:\n"
+)
+
+parsed = urlparse(redirected_url)
+code = parse_qs(parsed.query).get("code", [None])[0]
+
+if not code:
+    raise Exception("No authorization code found in URL")
+
+token_response = requests.post(
+    "https://www.linkedin.com/oauth/v2/accessToken",
+    data={
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": REDIRECT_URI,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+    },
+)
+
+print("\nResponse:")
+print(token_response.json())
